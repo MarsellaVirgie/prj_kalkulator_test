6d8b67b0aefd3c1cdf799843cf0556e4932e857f
marsellavirgie <marsellavirgie27@mhs.ac.id>
2023-10-12 23:29:36 +0700

    Mengunggah file proyek

---
+++ bin/prj_kalkulator_test.dart
@@ -0,0 +1,44 @@
+import 'dart:io';
+
+void main() {
+  print('Pilih Operator:');
+ print('1. Penjumlahan  (+)');
+ print('2. Pengurangan  (-)');
+ print('3. Perkalian    (x)');
+ print('4. Pembagian    (:)');
+
+ int operation = int.parse(stdin.readLineSync()!);
+
+ print('Masukan angka pertama :');
+ double num1 = double.parse(stdin.readLineSync()!);
+
+ print('Masukan angka kedua   :');
+ double num2 = double.parse(stdin.readLineSync()!);
+
+ double result;
+
+ switch (operation) {
+    case 1:
+      result = num1 + num2;
+      break;
+    case 2:
+      result = num1 - num2;
+      break;
+    case 3:
+      result = num1 * num2;
+      break;
+    case 4:
+      if (num2 == 0) {
+        print('Error! Pembagian bernilai 0 tidak dibolehkan.');
+        return;
+      }
+      result = num1 / num2;
+      break;
+    default:
+      print('Operator tidak tersedia.');
+      return;
+ }
+
+ print('Hasil: $result');
+
+}
---
+++ test/prj_kalkulator_test_test.dart
@@ -0,0 +1,8 @@
+import 'package:prj_kalkulator_test/prj_kalkulator_test.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
